612f12e7dfb4cab53dfac539a799b1d354a44c7a
Jagannathan Raman <jag.raman@oracle.com>
2024-05-06 16:38:01 -0400

    corim: ovmf changes

--- comid/referencevalue.go
+++ comid/referencevalue.go
@@ -4,16 +4,22 @@
 package comid
 
 import "fmt"
+import "github.com/jraman567/sev-snp-measure-go/ovmf"
 
 type ReferenceValue struct {
-	_            struct{}     `cbor:",toarray"`
-	Environment  Environment  `json:"environment"`
-	Measurements Measurements `json:"measurements"`
+	_                struct{}             `cbor:",toarray"`
+	Environment      Environment          `json:"environment"`
+	Measurements     Measurements         `json:"measurements"`
+	OvmfMetadata	 ovmf.MetadataWrapper `json:"ovmfmetadata"`
 }
 
 func (o ReferenceValue) Valid() error {
 	if err := o.Environment.Valid(); err != nil {
 		return fmt.Errorf("environment validation failed: %w", err)
+	}
+
+	if o.Measurements.Empty() && len(o.OvmfMetadata.OVMFHash) == 0 {
+		return fmt.Errorf("Both measurements and ovmfmetadata fields can't be empty")
 	}
 
 	if err := o.Measurements.Valid(); err != nil {
